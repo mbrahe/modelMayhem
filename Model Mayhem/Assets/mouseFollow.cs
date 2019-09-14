28c9ee98979014073d34294a4b386cb2fa19f4f8
Steven Heckman <heckman1@ad.umbc.edu>
2019-09-13 21:45:31 -0400

    mouse controls

---
+++ Model Mayhem/Assets/mouseFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseFollow : MonoBehaviour
+{
+    public GameObject mouseObject;
+    public Vector3 mousePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mouseObject.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, mouseObject.transform.position.z
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        //transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, mouseObject.transform.position.z);
+    }
+}
---
+++ Model Mayhem/Assets/protagMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class protagMove : MonoBehaviour
+{
+    public mouseFollow myMouse;
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+    public float xSpeed;
+    public float ySpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = myMouse.mousePosition;
+        /*
+        if(transform.position.x > mousePosition.x)
+        {
+            xSpeed -= .1f;
+        }
+        else if (transform.position.x < mousePosition.x)
+        {
+            xSpeed += .1f;
+        }
+        else if (transform.position.y > mousePosition.y)
+        {
+            ySpeed -= .1f;
+        }
+        else if (transform.position.y < mousePosition.y)
+        {
+            ySpeed += .1f;
+        }
+        */
+        //transform.position = Vector2.Lerp(transform.position, mousePosition*2, moveSpeed);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        //transform.position = new Vector3(transform.position.x + xSpeed, transform.position.y + ySpeed, transform.position.z);
+    }
+}
