257c8d1e58bc4317fab28583c7b87acb26f6e3f2
Matt Brahe <bajona1@ad.umbc.edu>
2019-11-01 12:49:01 -0400

    added pedestrians

---
+++ Model Mayhem/Assets/Scenes/V3/NavController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavController : MonoBehaviour
+{
+    public Transform target;
+    public float acceleration;
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = ((Vector2)target.position - (Vector2)transform.position).normalized;
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 2);
+
+        /*if (hit.collider != null)
+        {
+            direction = (Vector2) Vector3.Cross((Vector3) direction, Vector3.forward);
+            direction = direction.normalized;
+        }*/
+
+        rb.AddForce(direction * acceleration);
+
+    }
+}
---
+++ Model Mayhem/Assets/Scenes/V3/PathController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathController : MonoBehaviour
+{
+    public Transform[] targets;
+    public float targetCloseness = 1f;
+
+    int targetIndex;
+    NavController nav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavController>();
+        nav.target = targets[targetIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(((Vector2) transform.position - (Vector2) nav.target.position).magnitude) < targetCloseness)
+        {
+            if (targetIndex < targets.Length - 1)
+            {
+                targetIndex++;
+            } else
+            {
+                targetIndex = 0;
+            }
+
+            nav.target = targets[targetIndex];
+        }
+    }
+}
